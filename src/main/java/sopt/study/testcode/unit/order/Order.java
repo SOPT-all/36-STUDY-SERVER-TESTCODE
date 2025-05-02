b824680d32dac8a0030c2c6923f8532ffabe3673
kaswhy <sxshx818@naver.com>
2025-05-02 11:38:09 +0900

    feat(order): 주문/키오스크 관련 로직 작성

---
+++ src/main/java/sopt/study/testcode/unit/order/Order.java
@@ -0,0 +1,15 @@
+package sopt.study.testcode.unit.order;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import sopt.study.testcode.unit.beverage.Beverage;
+
+@Getter
+@RequiredArgsConstructor
+public class Order {
+
+    private final LocalDateTime orderDateTime;
+    private final List<Beverage> beverages;
+}
