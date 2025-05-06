114c55571840138e287c558a8aee3b71dbd666ac
hyeumm <20221159@sungshin.ac.kr>
2025-05-06 19:05:48 +0900

    [Feature] 스터디 1주차 실습 진행

---
+++ src/test/java/sopt/study/testcode/cafekiosk/CafeKioskApplicationTests.java
@@ -0,0 +1,13 @@
+package sopt.study.testcode.cafekiosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeKioskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
