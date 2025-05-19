9aa4b371c16659cf1d2a91033323906ff1869ed9
kymin <park48664866@gmail.com>
2025-05-19 11:44:36 +0900

    test: 2주차 강의 예제 테스트 코드 작성

--- src/test/java/sopt/study/testcode/kyongmin/spring/domain/product/ProductRepositoryTest.java
+++ src/test/java/sopt/study/testcode/kyongmin/spring/domain/product/ProductRepositoryTest.java
@@ -59,4 +59,43 @@ class ProductRepositoryTest {
 				tuple("002", "카페라떼", HOLD)
 			);
 	}
+
+	@Test
+	@DisplayName("상품번호 리스틀로 상품들을 조회한다.")
+	void findAllByProductNumberIn() {
+		// given
+		Product product1 = Product.builder()
+			.productNumber("001")
+			.type(HANDMADE)
+			.sellingStatus(SELLING)
+			.name("아메리카노")
+			.price(4000)
+			.build();
+		Product product2 = Product.builder()
+			.productNumber("002")
+			.type(HANDMADE)
+			.sellingStatus(HOLD)
+			.name("카페라떼")
+			.price(4500)
+			.build();
+		Product product3 = Product.builder()
+			.productNumber("003")
+			.type(HANDMADE)
+			.sellingStatus(STOP_SELLING)
+			.name("팥빙수")
+			.price(7000)
+			.build();
+		productRepository.saveAll(List.of(product1, product2, product3));
+
+		// when
+		List<Product> products = productRepository.findAllByProductNumberIn(List.of("001", "002"));
+
+		// then
+		assertThat(products).hasSize(2)
+			.extracting("productNumber", "name", "sellingStatus") // 원하는 필드만 추출
+			.containsExactlyInAnyOrder(    // 아래의 튜플이 추출 결과에 존재하는지 확인
+				tuple("001", "아메리카노", SELLING),
+				tuple("002", "카페라떼", HOLD)
+			);
+	}
 }
