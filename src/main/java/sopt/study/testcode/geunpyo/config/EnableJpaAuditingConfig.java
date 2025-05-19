4a7aff442d9885d419d5974016da7aa7f62c6122
rootTiket <l20078829@gmail.com>
2025-05-19 15:11:01 +0900

    config 분리

--- src/main/java/sopt/study/testcode/geunpyo/config/EnableJpaAuditingConfig.java
+++ src/main/java/sopt/study/testcode/geunpyo/config/EnableJpaAuditingConfig.java
@@ -1,9 +1,7 @@
 package sopt.study.testcode.geunpyo.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
-@EnableJpaAuditing
 public class EnableJpaAuditingConfig {
 }
