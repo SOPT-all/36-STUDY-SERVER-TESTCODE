73ccb51ba0d9aa45215ab6e38c9f31d1a50fdde6
2hyunjinn <huenjin04@naver.com>
2025-05-23 19:57:27 +0900

    [feat] 주문 기능 구현

---
+++ src/main/java/sopt/study/testcode/hyunjin/spring/api/service/order/OrderService.java
@@ -0,0 +1,32 @@
+package sopt.study.testcode.hyunjin.spring.api.service.order;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import sopt.study.testcode.hyunjin.spring.api.controller.order.OrderCreateRequest;
+import sopt.study.testcode.hyunjin.spring.api.service.order.response.OrderResponse;
+import sopt.study.testcode.hyunjin.spring.domain.order.Order;
+import sopt.study.testcode.hyunjin.spring.domain.order.OrderRepository;
+import sopt.study.testcode.hyunjin.spring.domain.product.Product;
+import sopt.study.testcode.hyunjin.spring.domain.product.ProductRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class OrderService {
+
+    private final ProductRepository productRepository;
+    private final OrderRepository orderRepository;
+
+    public OrderResponse createOrder(OrderCreateRequest request, LocalDateTime now) {
+        List<String> productNumbers = request.getProductNumbers();
+
+        List<Product> products = productRepository.findAllByProductNumberIn(productNumbers);
+
+        Order order = Order.create(products, now);
+        Order savedOrder = orderRepository.save(order);
+
+        return OrderResponse.of(savedOrder);
+    }
+}
