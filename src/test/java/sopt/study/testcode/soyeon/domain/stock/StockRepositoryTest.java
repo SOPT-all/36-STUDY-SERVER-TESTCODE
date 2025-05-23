67fde72ad5792becbfb29788405da2b5f048ce09
kaswhy <sxshx818@naver.com>
2025-05-23 21:48:12 +0900

    chore(domain): 테스트 코드 정렬

--- src/test/java/sopt/study/testcode/soyeon/domain/stock/StockRepositoryTest.java
+++ src/test/java/sopt/study/testcode/soyeon/domain/stock/StockRepositoryTest.java
@@ -2,18 +2,12 @@ package sopt.study.testcode.soyeon.domain.stock;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.groups.Tuple.tuple;
-import static org.junit.jupiter.api.Assertions.*;
-import static sopt.study.testcode.soyeon.domain.product.ProductSellingStatus.HOLD;
-import static sopt.study.testcode.soyeon.domain.product.ProductSellingStatus.SELLING;
-import static sopt.study.testcode.soyeon.domain.product.ProductSellingStatus.STOP_SELLING;
-import static sopt.study.testcode.soyeon.domain.product.ProductType.HANDMADE;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import sopt.study.testcode.soyeon.domain.product.Product;
 
 @DataJpaTest
 class StockRepositoryTest {
