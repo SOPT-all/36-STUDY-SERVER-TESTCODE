c699bf22e015b68d1c4ca77d75d4a20810cfc13d
kaswhy <sxshx818@naver.com>
2025-05-30 10:45:11 +0900

    feat(product): 새로운 주문 생성 컨트롤러 구현

--- src/main/java/sopt/study/testcode/soyeon/api/controller/product/ProductController.java
+++ src/main/java/sopt/study/testcode/soyeon/api/controller/product/ProductController.java
@@ -3,7 +3,9 @@ package sopt.study.testcode.soyeon.api.controller.product;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sopt.study.testcode.soyeon.api.controller.product.request.ProductCreateRequest;
 import sopt.study.testcode.soyeon.api.service.product.ProductService;
 import sopt.study.testcode.soyeon.api.service.product.response.ProductResponse;
 
@@ -12,6 +14,11 @@ import sopt.study.testcode.soyeon.api.service.product.response.ProductResponse;
 public class ProductController {
 
     private final ProductService productService;
+
+    @PostMapping("/api/v1/products/new")
+    public void createProduct(ProductCreateRequest request) {
+        productService.createProduct(request);
+    }
 
     @GetMapping("/api/v1/products/selling")
     public List<ProductResponse> getSellingProducts() {
