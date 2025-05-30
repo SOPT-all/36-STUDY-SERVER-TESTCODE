85bdb2b38ec0ec6d278365782e522b48e942d338
kaswhy <sxshx818@naver.com>
2025-05-30 10:45:45 +0900

    feat(product): 새로운 주문 생성 서비스 구현

--- src/main/java/sopt/study/testcode/soyeon/api/service/product/ProductService.java
+++ src/main/java/sopt/study/testcode/soyeon/api/service/product/ProductService.java
@@ -4,17 +4,31 @@ import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sopt.study.testcode.soyeon.api.controller.product.request.ProductCreateRequest;
 import sopt.study.testcode.soyeon.api.service.product.response.ProductResponse;
 import sopt.study.testcode.soyeon.domain.product.Product;
 import sopt.study.testcode.soyeon.domain.product.ProductRepository;
 import sopt.study.testcode.soyeon.domain.product.ProductSellingStatus;
 
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class ProductService {
 
     private final ProductRepository productRepository;
 
+    @Transactional
+    public ProductResponse createProduct(ProductCreateRequest request) {
+        String nextProductNumber = createNextProductNumber();
+
+        Product product = request.toEntity(nextProductNumber);
+        Product savedProduct = productRepository.save(product);
+
+        return ProductResponse.of(savedProduct);
+    }
+
+    @Transactional(readOnly = true)
     public List<ProductResponse> getSellingProducts() {
         List<Product> products = productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay());
 
@@ -22,4 +36,18 @@ public class ProductService {
                 .map(ProductResponse::of)
                 .collect(Collectors.toList());
     }
+
+    private String createNextProductNumber() {
+        String latestProductNumber = productRepository.findLatestProduct();
+        if(latestProductNumber == null) {
+            return "001";
+        }
+
+        int latestProductNumberInt = Integer.parseInt(latestProductNumber);
+        int nextProductNumberInt = latestProductNumberInt + 1;
+
+        // 9 -> 009, 10 -> 010
+        return String.format("%03d", nextProductNumberInt);
+    }
+
 }
