007f513fe73e3399a2a5abd72f9c39e6790591b3
kaswhy <sxshx818@naver.com>
2025-05-30 12:22:19 +0900

    refactor(product): 신규 상품 등록 시 공통 응답 사용

--- src/main/java/sopt/study/testcode/soyeon/api/controller/product/ProductController.java
+++ src/main/java/sopt/study/testcode/soyeon/api/controller/product/ProductController.java
@@ -1,10 +1,13 @@
 package sopt.study.testcode.soyeon.api.controller.product;
 
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import sopt.study.testcode.soyeon.api.ApiResponse;
 import sopt.study.testcode.soyeon.api.controller.product.request.ProductCreateRequest;
 import sopt.study.testcode.soyeon.api.service.product.ProductService;
 import sopt.study.testcode.soyeon.api.service.product.response.ProductResponse;
@@ -16,8 +19,8 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping("/api/v1/products/new")
-    public void createProduct(ProductCreateRequest request) {
-        productService.createProduct(request);
+    public ApiResponse<ProductResponse> createProduct(@Valid @RequestBody ProductCreateRequest request) {
+        return ApiResponse.ok(productService.createProduct(request));
     }
 
     @GetMapping("/api/v1/products/selling")
