441b485cf56a7028150fcce907da583b991f4dcd
rootTiket <l20078829@gmail.com>
2025-06-24 02:04:40 +0900

    섹션 7 실습

---
+++ src/test/java/sopt/study/testcode/geunpyo/spring/api/service/MailServiceTest.java
@@ -0,0 +1,53 @@
+package sopt.study.testcode.geunpyo.spring.api.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+
+import sopt.study.testcode.geunpyo.spring.api.service.mail.MailService;
+import sopt.study.testcode.geunpyo.spring.client.mail.MailSendClient;
+import sopt.study.testcode.geunpyo.spring.domain.history.mail.MailSendHistory;
+import sopt.study.testcode.geunpyo.spring.domain.history.mail.MailSendHistoryRepository;
+
+@ExtendWith(MockitoExtension.class)
+class MailServiceTest {
+
+    @Mock
+    private MailSendClient mailSendClient;
+
+    @Mock
+    private MailSendHistoryRepository mailSendHistoryRepository;
+
+    @InjectMocks
+    private MailService mailService;
+
+    @DisplayName("메일 전송 테스트")
+    @Test
+    void sendMail() {
+        // given
+//        Mockito.when(mailSendClient.sendEmail(anyString(), anyString(), anyString(), anyString()))
+//            .thenReturn(true);
+        given(mailSendClient.sendEmail(anyString(), anyString(), anyString(), anyString()))
+            .willReturn(true);
+
+//        doReturn(true)
+//            .when(mailSendClient)
+//            .sendEmail(anyString(), anyString(), anyString(), anyString());
+
+        // when
+        boolean result = mailService.sendMail("", "", "", "");
+
+        // then
+        assertThat(result).isTrue();
+        verify(mailSendHistoryRepository, times(1)).save(any(MailSendHistory.class));
+    }
+
+}
